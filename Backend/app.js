397745816c12c3fb88c8b6af3b365bcbc3b6cb50
gauravg411 <86140468+gauravg411@users.noreply.github.com>
2025-01-20 12:40:31 +0530

    ddasf

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://localhost:27017/taskmanagement')
 // Routes
 
 app.get('/', (req, res) => {
-  res.send('API is running get it');
+  res.send('API is running get iddd');
 })
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
